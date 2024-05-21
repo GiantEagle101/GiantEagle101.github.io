9b60bf5ea32115c56a4da90b21f0921a11cb7bcd
tadene <adetayo.adenekan@gianteagle.com>
2024-05-21 15:13:34 -0400

    login

--- scripts.js
+++ scripts.js
@@ -1,10 +1,16 @@
-function userLogin(){
-    let inputElement= document.getElementById('username-input');
-    let inputValue= inputElement.value;
-    let newMessage= document.createElement("div");
-    newMessage.textContent= inputValue;
-    console.log(newMessage.textContent);
-}
+function userLogin(username, password){
+    try{
+            let inputElement= document.getElementById('username-input');
+            let inputValue= inputElement.value;
+            let newMessage= document.createElement("div");
+            newMessage.textContent= inputValue;
+            console.log(newMessage.textContent);
+        } catch (error){
+            console.log("wrong username or password, try again!", error.message);
+        }
+
+    }
+   
 
 function newItem(){
     
