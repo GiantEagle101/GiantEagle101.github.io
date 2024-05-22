2b6e44a4d24e8f2f504666a9965741c47419ade6
tadene <adetayo.adenekan@gianteagle.com>
2024-05-22 10:15:35 -0400

    login user object

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
+function userLogin(username, password){
+    try{ // catch errors for username and password during login 
+            //let inputElement= document.getElementById('username-input');
+            //let inputValue= inputElement.value;
+            //let newMessage= document.createElement("div");
+            //newMessage.textContent= inputValue;
+            console.log(newMessage.textContent);
+            const login= new userLogin();
+            login.username= "username";
+            login.password= "password";
+            login.errorMessage= "error-message";
+        } catch (error){
+            console.log("wrong username or password, try again!", error.message);
+        }
+loginButton.addEventListener("click");{
+    const userName= username.value;
+    const passWord= password.value;
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const loginButton = document.getElementById("sign-in");
@@ -28,4 +45,4 @@ const login = () => {
 loginButton.addEventListener("click", login);
 
 
-
+}
