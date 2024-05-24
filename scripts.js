d6a5e67e7335cba1f2e647e89f8c592e54c70212
irwin <irwin.jiang@gianteagle.com>
2024-05-24 10:38:16 -0400

    Merged

--- scripts.js
+++ scripts.js
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-//function userLogin(username, password)
-    //try{ // catch errors for username and password during login 
-//function userLogin(username, password){
-   // try{ // catch errors for username and password during login 
-            //let inputElement= document.getElementById('username-input');
-            //let inputValue= inputElement.value;
-            //let newMessage= document.createElement("div");
-            //newMessage.textContent= inputValue;
-            //console.log(newMessage.textContent);
-            //const login= new userLogin();
-            //login.username= "username";
-            //login.password= "password";
-            //login.errorMessage= "error-message";
-        //} catch (error){
-            //console.log("wrong username or password, try again!", error.message);
-       // }
-           // console.log(newMessage.textContent);
-            //const login= new userLogin();
-            //login.username= "username";
-           // login.password= "password";
-            //login.errorMessage= "error-message";
-       // } catch (error){
-            //console.log("wrong username or password, try again!", error.message);
-       // }
-//loginButton.addEventListener("click");{
-  //  const userName= username.value;
-    //const passWord= password.value;
-=======
->>>>>>> e2a02acd10dc35f2b04e744fe7aa4b97b812a893
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const loginButton = document.getElementById("sign-in");
